32cfa1d1e6d187a2eebb109b9f7e789f32742f37
miksik98 <49250846+miksik98@users.noreply.github.com>
2019-04-17 02:22:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-import vision.state_reader as reader
+#import vision.state_reader as reader
 from models.computer_player import ComputerPlayer
 from models.human_player import HumanPlayer
 from models.state import State
 
-reader.init()
+#reader.init()
 
-players = [HumanPlayer("Humek"), ComputerPlayer("Compek")]
+players = [HumanPlayer("O", "Humek"), ComputerPlayer("X", "Compek")]
 turn = 0
-state = State([[0,0,0], [0,0,0], [0,0,0]])
+state = State([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
 while True:
-    frame = reader.get_state()
-    print(frame)
-    players[turn].move(state)
-    turn = (turn + 1) % 2
+    #frame = reader.get_state()
+    #print(frame)
+    print(state)
 
-reader.destroy()
+    try:
+        players[turn].move(players[(turn+1) % 2], state)
+    except TypeError as te:
+        print(te)
+        break
+
+    if state.winning(players[turn].symbol):
+        print(players[turn].name+" WINS")
+        print(state)
+        break
+    if state.tie():
+        print("TIE")
+        print(state)
+        break
+
+    turn = (turn + 1) % 2
+#reader.destroy()
