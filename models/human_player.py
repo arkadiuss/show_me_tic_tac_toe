3855d5cf0dd3584653a551342b738524a3acdb55
miksik98 <49250846+miksik98@users.noreply.github.com>
2019-04-17 02:18:07 +0200

    Update human_player.py

--- models/human_player.py
+++ models/human_player.py
@@ -4,8 +4,21 @@ from models.state import State
 
 class HumanPlayer(Player):
 
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, symbol, name):
+        super().__init__(symbol, name)
 
-    def move(self, state: State):
-        return input()
+    def move(self, computer, state: State):
+        correct_input = False
+        while not correct_input:
+            movement = input()
+            if len(movement) > 1:
+                raise TypeError("Wrong input!\n")
+            if ord(movement) < 48 or ord(movement) > 56:
+                raise TypeError("Wrong input!\n")
+            movement = int(movement, 10)
+            if state.board[movement // len(state.board)][movement % len(state.board)] != 0:
+                print("This field is busy!\nWrite correct number of field.\n")
+                print(state)
+            else:
+                correct_input = True
+        state.board[movement//len(state.board)][movement % len(state.board)] = self.symbol
