ab40152548bff553f8553f19f0ea3e9d2fa7b041
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-05-14 16:08:09 +0200

    pasting lines

--- vision/state_reader.py
+++ vision/state_reader.py
@@ -1,10 +1,31 @@
+from copy import deepcopy
 import cv2
 import vision.image_processor as ip
+import numpy as np
 
 
 def init():
     global cap
     cap = cv2.VideoCapture(0)
+
+
+def _paste_non_zero(src, dest):
+    s = deepcopy(src)
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if np.all(dest[i][j] == 0):
+                s[i][j] = dest[i][j]
+    return s
+
+
+def _create_lines_image(frame, lines):
+    res = np.zeros(frame.shape)
+    for i in lines[0]:
+        res[i, :] = 127
+
+    for i in lines[1]:
+        res[:, i] = 127
+    return res
 
 
 def get_state():
@@ -16,16 +37,15 @@ def get_state():
     # for testing
     frame = cv2.imread('out2.png')
     binary = ip.to_binary_color(frame)
-    sq = ip.split_to_squares(binary)
+    sq, lines = ip.split_to_squares(binary)
     state = []
     for row in sq:
         state.append([])
         for column in row:
             state[-1].append(ip.recognize_shape(column))
-    print(state)
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', _paste_non_zero(frame, _create_lines_image(frame, lines)))
     cv2.waitKey(1) & 0xFF == ord('q')  # required to show
-    return frame
+    return state
 
 
 def destroy():
