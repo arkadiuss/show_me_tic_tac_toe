e0b586f26086648ade63db9c28183288c9fafb25
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-05-27 21:59:39 +0200

    speech and tests

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import vision.state_reader as reader
 from strategies.console_game import ConsoleGame
 from strategies.vision_game import VisionHumanPlayersGame
-
 
 # strategy = ConsoleGame()
 strategy = VisionHumanPlayersGame()
@@ -19,11 +17,3 @@ while not state.end():
     turn = (turn + 1) % 2
 
 print(state.result())
-
-
-# VISION TEST
-# reader.init()
-#
-# while 1:
-#     frame = reader.get_state()
-# reader.destroy()
--- players/vision/computer_player.py
+++ players/vision/computer_player.py
@@ -3,6 +3,22 @@ from players.ai.square_board import optimum_move
 from players.player import Player
 from players.vision.common import wait_for_move
 from state.state import State
+import pyttsx3
+
+
+def say(engine, text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def _number_name(num):
+    if num == 1:
+        return 'first'
+    elif num == 2:
+        return 'second'
+    elif num == 3:
+        return 'third'
+    return num
 
 
 def _symbol_name(symbol):
@@ -15,16 +31,20 @@ def _symbol_name(symbol):
 
 class ComputerPlayer(Player):
 
+    def __init__(self, symbol, name):
+        super().__init__(symbol, name)
+        self.speech_engine = pyttsx3.init()
+
     def move(self, state: State, opponent):
-        print("{0}: Now it's my turn! You will lose!".format(self.name))
+        say(self.speech_engine, "Now it's my turn! You will lose!")
         movement = optimum_move(state.board(), opponent.symbol, self.symbol, self.symbol).index
         r, c = movement
-        print("Please put {0} on {1},{2}".format(_symbol_name(self.symbol), r, c))
+        say(self.speech_engine, "Please put {0} in {1} row and {2} column".format(_symbol_name(self.symbol), _number_name(r+1), _number_name(c + 1)))
         try:
             dr, dc = wait_for_move(state, self.symbol)
             if dr != r or dc != c:
                 raise MoveError
         except MoveError:
-            print("You tried to lie me. I won't play with you!")
+            say(self.speech_engine, "You tried to lie me. I won't play with you!")
             raise MoveError
-        print("{0}: Thank you".format(self.name))
+        say(self.speech_engine, "Thank you")
